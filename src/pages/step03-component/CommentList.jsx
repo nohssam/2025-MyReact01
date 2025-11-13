e4db6255c291a16f425d14e08066a3eddbf6cfa4
nohssam <nohssam73@gmail.com>
2025-11-13 17:51:40 +0900

    CommentList  수정

--- src/pages/step03-component/CommentList.jsx
+++ src/pages/step03-component/CommentList.jsx
@@ -26,6 +26,8 @@ export default function CommentList(params) {
     return(
         <div>
             {
+                //  - 반복되는 컴포넌트를 렌더링하기 위하여 자바스크립트 배열의 내장 함수인 map()을 사용 한다.  
+                // - 파라미터로 전달된 함수를 사용하여 배열 내 각 요소를 원하는 규칙에 따라 변환한 후 새로운 배열 생성 한다.
                 // 각 항목을 구분하는 key 속성이 필요하다.
                 comments.map((k,idx)=>{
                     return(
